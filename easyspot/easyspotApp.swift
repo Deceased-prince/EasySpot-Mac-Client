d262ce58f297ddd9e029bce6bb268d3780ff2814
Joshua Mendoza <deceasedprince@the-revenant.nexus>
2026-04-05 12:12:49 -0400

    Initial Commit

---
+++ easyspot/easyspotApp.swift
@@ -0,0 +1,17 @@
+//
+//  easyspotApp.swift
+//  easyspot
+//
+//  Created by Joshua Mendoza on 4/5/26.
+//
+
+import SwiftUI
+
+@main
+struct easyspotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
